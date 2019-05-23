f677c4bc76c9ea9dcce15475cc0e9beb6cc8fb99
diotridia <dio.tridia@gmail.com>
2019-05-23 10:37:02 +0700

    Updates

---
+++ precache-manifest.b4cd856ac4b8d13691104a867d45ec28.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15273b1804069ed269b1adbed88ceee8",
+    "url": "/responsiweb/index.html"
+  },
+  {
+    "revision": "62ab0030656aa128b53e",
+    "url": "/responsiweb/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/responsiweb/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "62ab0030656aa128b53e",
+    "url": "/responsiweb/static/js/main.dbffd5a8.chunk.js"
+  },
+  {
+    "revision": "adae9c95a9a51b7aacd3",
+    "url": "/responsiweb/static/js/runtime~main.a04c41a8.js"
+  }
+]);
